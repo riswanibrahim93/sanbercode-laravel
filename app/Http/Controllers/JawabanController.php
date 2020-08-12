d9f9f5287618c1b6de0ec7317f8519a80f33fcaf
riswanibrahim93 <57494835+riswanibrahim93@users.noreply.github.com>
2020-08-13 01:15:16 +0700

    Add files via upload
    tugas eloquent-relationships 2 selesai

---
+++ app/Http/Controllers/JawabanController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Jawaban;
+use Illuminate\Support\Facades\Auth;
+
+class JawabanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
+    public function index()
+    {
+        //
+        // $user = Auth::user();
+        $jawabans = Jawaban::all();
+        // dd($jawabans);
+        return view('WebDiskusi.jawaban.jawaban', compact('jawabans'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $jawaban = Jawaban::where('id_jawaban', $id)->first();
+        $pertanyaan = $jawaban->pertanyaan;
+        // dd($pertanyaan);
+        return view('WebDiskusi.jawaban.jawaban_pertanyaan', ['pertanyaan'=>$pertanyaan]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
