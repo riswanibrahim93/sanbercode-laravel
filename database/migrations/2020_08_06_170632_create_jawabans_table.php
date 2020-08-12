8ea1b08d675b09023d28d5fefaca8b8ab1c95ec4
riswanibrahim93 <57494835+riswanibrahim93@users.noreply.github.com>
2020-08-13 01:18:39 +0700

    Update 2020_08_06_170632_create_jawabans_table.php
    tugas eloquent-relationships 2 selesai

--- database/migrations/2020_08_06_170632_create_jawabans_table.php
+++ database/migrations/2020_08_06_170632_create_jawabans_table.php
@@ -13,14 +13,14 @@ class CreateJawabansTable extends Migration
      */
     public function up()
     {
-        Schema::create('jawabans', function (Blueprint $table) {
+       Schema::create('jawabans', function (Blueprint $table) {
             $table->bigIncrements('id_jawaban');
             $table->string('isi');
             $table->date('tanggal_dibuat');
             $table->date('tanggal_diperbaharui');
-            $table->unsignedBigInteger('profile_id');
+            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('pertanyaan_id');
-            $table->foreign('profile_id')->references('id_profile')->on('profiles');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('pertanyaan_id')->references('id_pertanyaan')->on('pertanyaans');
         });
     }
