7a1adb53ecec341279c5eddaaf203da2d59f2e82
riswanibrahim93 <57494835+riswanibrahim93@users.noreply.github.com>
2020-08-13 01:23:32 +0700

    Update create.blade.php
    tugas eloquent-relationships 2 selesai

--- resources/views/WebDiskusi/create.blade.php
+++ resources/views/WebDiskusi/create.blade.php
@@ -26,6 +26,10 @@
           <label for="isi">Isi</label>
           <textarea class="form-control @error('isi') is-invalid @enderror" id="isi"  name="isi" value="{{ old('isi') }}"></textarea>
         </div>
+        <div class="form-group">
+          <label for="tag">Tags</label>
+          <input type="text" class="form-control @error('tag') is-invalid @enderror" id="tag"  name="tag" placeholder="Masukkan tag, bila lebih dari satu pisahkan dengan koma" value="{{ old('tag') }}">
+        </div>
         <button type="submit" class="btn btn-danger ">Tambah</button>
         <a class="btn btn-primary float-right" href="/pertanyaan">kembali</a>
       </form>
